f68d6619a41437dee9d8b5c508b29d2478efd997
PetNik48 <neket20021@mail.ru>
2025-04-29 12:22:23 +0300

    Fix changelog gen

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,1 +1,5 @@
-git log --pretty=format:'- %s (%an)' origin/main..HEAD > CHANGELOG.md
+#!/bin/bash
+# Generate Changelog
+echo "# Changelog" > CHANGELOG.md
+echo "" >> CHANGELOG.md
+git log origin/main --pretty=format:'- %s (%an, %ad)' --date=short >> CHANGELOG.md
